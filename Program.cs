b93b18bb4fb750312a4cfbbc08b4ea6b1957f9f9
Isaac <isaacgeazy@gmail.com>
2021-03-01 16:24:40 -0300

    altera descrição e mensagem exibida

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aula_26_02_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
