95943989bead134b35d94d5f0c0744933c72381d
Isaac <isaacgeazy@gmail.com>
2021-03-01 17:43:13 -0300

    Atividade música

--- Program.cs
+++ Program.cs
@@ -6,7 +6,94 @@ namespace aula_26_02_2021
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Leal ");
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Leal, leal (só pra você)");
+            Console.WriteLine("Leal");
+            Console.WriteLine();
+            Console.WriteLine("Leal, leal");
+            Console.WriteLine("Eu quero ser leal, enquanto nosso lance for real");
+            Console.WriteLine("Só pra você, leal, leal");
+            Console.WriteLine("Eu quero ser leal, enquanto nosso lance for real");
+            Console.WriteLine("Só pra você");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("O baile tava a uva, Don Juan no palco");
+            Console.WriteLine("É, eu me envolvendo com uma outra gata");
+            Console.WriteLine("Você chegou meio minha dona, eu estilo cachorro");
+            Console.WriteLine("E olhou assim como quem diz, ó, dá a pata");
+            Console.WriteLine("Mas coleira em pitbull, garota, é sacanagem");
+            Console.WriteLine("E se tu vem pro meu canil saca, nós se acasala");
+            Console.WriteLine("Me disse que tem pedigree, mas me morde e me arranha");
+            Console.WriteLine("Impossível de adestrar esse amor vira-lata");
+            Console.WriteLine("Vai ser nós contra o mundo sempre");
+            Console.WriteLine("Contra toda caretice, de mãos dadas");
+            Console.WriteLine("Minha Queen Latifah, o trono é seu, se lembre");
+            Console.WriteLine("'Tá na pele que Katy Perry não se compara");
+            Console.WriteLine("Fato que a vida que eu levo me consome");
+            Console.WriteLine("Ahn, e é claro que sinto saudades");
+            Console.WriteLine("Meu som molha elas, procura é alta e eu me envolvo pouco");
+            Console.WriteLine("E você disse que não vê maldade");
+            Console.WriteLine("Ela me diz que faz cinema, minha vida é um filme");
+            Console.WriteLine("Nós dois é combinação que vale Oscar");
+            Console.WriteLine("Oscar Niemeyer desenhou suas curvas");
+            Console.WriteLine("Parece que eu mandei fazer");
+            Console.WriteLine("Juro que 'tá do jeito que o pai gosta");
+            Console.WriteLine("Falar em juras, não te prometo nada");
+            Console.WriteLine("Só que sempre que eu for eu volto");
+            Console.WriteLine("Às vezes a paixão sufoca");
+            Console.WriteLine("Falar em juras, não te prometo nada, só que eu te amo e fé");
+            Console.WriteLine("Fé, o resto é fofoca");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Leal, leal");
+            Console.WriteLine("Eu quero ser leal, enquanto nosso lance for real");
+            Console.WriteLine("Só pra você, leal, leal");
+            Console.WriteLine("Eu quero ser leal, enquanto nosso lance for real");
+            Console.WriteLine("Só pra você");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Ela deitada é a criação do mundo no sétimo dia");
+            Console.WriteLine("Deus descansando");
+            Console.WriteLine("Ela de pé é Estátua da Liberdade, me faz crer que eu posso tudo");
+            Console.WriteLine("Deus me testando");
+            Console.WriteLine("Eu creio que eu posso voar, no ar, te quero nua");
+            Console.WriteLine("No meu lar, 'cê me diz sou toda sua");
+            Console.WriteLine("Suar na cama, zuar, transar na rua");
+            Console.WriteLine("Fazer do sol á noite e da lua o dia");
+            Console.WriteLine("Me chamou de cabeça dura e é fato, sem maldade");
+            Console.WriteLine("Sei que sou em todos sentidos");
+            Console.WriteLine("A gente briga só pra se acertar de novo");
+            Console.WriteLine("Fica mais quente, né? Nós dois sentimos");
+            Console.WriteLine("Companheirismo é discordar e ir pro corre junto");
+            Console.WriteLine("É viajar horas de estrada sem perder o assunto");
+            Console.WriteLine("Tipo que eu nem sei se ela vem mas guardar o assento");
+            Console.WriteLine("Coisas da vida, se for sua ex ela vira sua amiga");
+            Console.WriteLine("Ouvindo Filipe Ret com o som no talo");
+            Console.WriteLine("Gritando que o mundo é nosso");
+            Console.WriteLine("Ouvindo Don L com ela no colo");
+            Console.WriteLine("Pensando que se foda o mundo");
+            Console.WriteLine("Nós construímos pontes indestrutíveis");
+            Console.WriteLine("Creio que ao seu lado tudo posso");
+            Console.WriteLine("Com você sinto coisas indescritíveis, fica comigo");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Leal, leal");
+            Console.WriteLine("Eu quero ser leal, enquanto nosso lance for real");
+            Console.WriteLine("Só pra você, leal, leal");
+            Console.WriteLine("Eu quero ser leal, enquanto nosso lance for real");
+            Console.WriteLine("Só pra você");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.Write("Djonga - Leal");
+           
+            
         }
     }
 }
